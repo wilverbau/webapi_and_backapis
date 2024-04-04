ae329f72754514d9c47bbaa90667aaf764823e37
toto <toto@toto.to>
2024-04-04 11:51:14 -0400

    Added Blazor project with GET actions.

---
+++ WebApiBackApis/BlazorServerApp/Models/WeatherForecastFull.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorServerApp.Models
+{
+    public class WeatherForecastFull
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+        public int Zip { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string State { get; set; }
+    }
+}
